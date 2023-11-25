21768649780a6e6d000aec26d43bb12fd372f8a5
akash-0233 <akashrajput0233@gmail.com>
2023-11-25 18:24:54 +0530

    Added SmartContract

---
+++ contracts/HelloTesting.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >0.5.0 <=0.9.0;
+
+
+contract HelloTesting {
+  function print() public pure returns(string memory){
+    return "HelloTesting";
+  }
+
+  function print1() public pure returns(string memory){
+    return "Check 2";
+  }
+}
